9a99a161d8d8374b12b98ccd6af5ae3d41197e09
Adam Kucharik <Adam@Adams-Air.netgear.com>
2016-05-07 16:47:09 -0400

    Refactor to take multiple parameters when instantiated

--- src/scripts/math/matrix2.js
+++ src/scripts/math/matrix2.js
@@ -1,83 +1,102 @@
 // Utilities
 var isArray = _.isArray;
 
+/**
+* Create 2x2 matrix from a series of values.
+* 
+* Represented like:
+* 
+* | a | b |
+* | c | d |
+*
+* @class Matrix2
+* @param {number} [a=1]
+* @param {number} [b=0]
+* @param {number} [c=0]
+* @param {number} [d=1]
+*//**
+* Create a 2x2 matrix from an array.
+* @class Matrix2
+* @param {Array} - The matrix values.
+*//**
+* Create a 2x2 matrix initialzed to its identity.
+* @class Matrix2
+*/
 class Matrix2 {
-    constructor (m) {
+    constructor (a, b, c, d) {
         /**
-        * The matrix value.
-        * @default [1,0,0,1]
+        * @property {number} a
+        * @default
         */
-        this.value = null;
-                    
-        if (!m) {
-            this.value = this.identity;
+        this.a = 1;
+        
+        /**
+        * @property {number} b
+        * @default
+        */
+        this.b = 0;
+        
+        /**
+        * @property {number} c
+        * @default
+        */
+        this.c = 0;
+        
+        /**
+        * @property {number} d
+        * @default
+        */
+        this.d = 1;
+        
+        if (arguments.length === 4) {
+            this.setTo(a, b, c, d);
         }
-        else if (isArray(m) && m.length === 4) {
-            this.value = m;
+        else if (isArray(a) && a.length === 4) {
+            this.setFromArray(a);
         }
-        else {
-            throw new Error('Cannot instantiate a matrix from an invalid array');
-        }
-    }
-    
-    /**
-    * Gets the number of columns.
-    */
-    get cols () {
-        return 2;
     }
     
     /**
     * Gets the determinant.
     */
     get determinant () {
-        var m = this.value;
-        var a = m[0], b = m[1],
-            c = m[2], d = m[3];
-    
-        return a * d - b * c;
-    }
-    
-    /**
-    * Gets the identity.
-    */
-    get identity () {
-        return [1, 0,
-                0, 1];
+        return this.a * this.d - this.b * this.c;
     }
     
     /**
     * Gets the inverse.
     */
     get inverse () {
-        var v = this.value;
-        var a = v[0], b = v[1],
-            c = v[2], d = v[3];
-        var m = new Matrix2([d, -b, -c, a])
+        var m = new Matrix2(this.d, -this.b, -this.c, this.a)
 
         return m.multiplyScalar(1 / m.determinant);
     }
     
     /**
-    * Gets the number of rows.
+    * Sets the matrix to the identity.
     */
-    get rows () {
-        return 2;
+    identity () {
+        return this.setTo(1, 0 , 0, 1);
     }
     
     /**
-    * Multiplies the matrix by one or more matrices.
-    * @param {Matrix2|Matrix2D|Array} m - One or more Matrices.
+    * Multiplies the matrix by another matrix.
+    * @param {Matrix2|Matrix2D} m - a matrix.
+    * @return {Matrix2} - The matrix.
+    *//**
+    * Multiplies the matrix by a list of matrices.
+    * @param {Array} m - A list of matrices.
     * @return {Matrix2} - The matrix.
     */
     multiply (m) {
+        var result;
+        
         if (isArray(m)) {
-            m.forEach(function (item) {
-                this.value = Matrix2.multiplyMatrices(this, item).value;
-            });
+            m.forEach(i => this.multiply(i));
         }
         else {
-            this.value = Matrix2.multiplyMatrices(this, m).value;
+            result = Matrix2.multiplyMatrices(this, m);
+            this.setFromArray(result.toArray());
         }
 
         return this;
@@ -91,40 +110,105 @@ class Matrix2 {
     * @return {Matrix2} - The multiplied matrix.
     */
     static multiplyMatrices (a, b) {
+        var m = new Matrix2();
+        
         if (a.cols === b.rows) {
-            var av = a.value;
-            var bv = b.value;
-            var nv = new Array(4);
-            var a11 = av[0], a12 = av[1],
-                a21 = av[2], a22 = av[3],
-                b11 = bv[0], b12 = bv[1],
-                b21 = bv[2], b22 = bv[3];
-
-            nv[0] = a11 * b11 + a12 * b21;
-            nv[1] = a11 * b12 + a12 * b22;
-            nv[2] = a21 * b11 + a22 * b21;
-            nv[3] = a21 * b12 + a22 * b22;
+            m.a = a.a * b.a + a.b * b.c;
+            m.b = a.a * b.b + a.b * b.d;
+            m.c = a.c * b.a + a.d * b.c;
+            m.d = a.c * b.b + a.d * b.d;
         }
         else {
             throw new Error('Cannot multiply incompatible matrices');
         }
 
-        return new Matrix2(nv);
+        return m;
     }
     
     /**
-    * Multiplies a matrix by a scalar.
+    * Multiplies the matrix by a scalar.
     * @param {number} s - The scalar.
-    * @return {Matrix2} - The scaled matrix.
+    * @return {Matrix2} - The matrix.
     */
     multiplyScalar (s) {
-        var v = this.value;
-        var a = v[0], b = v[1],
-            c = v[2], d = v[3];
-
-        a *= s; b *= s;
-        c *= s; d *= s;
+        this.a *= s;
+        this.b *= s;
+        this.c *= s;
+        this.d *= s;
 
         return this;
     }
+    
+    /**
+    * Sets the matrix from an array.
+    * @param {Array} array - The array of matrix values.
+    * @return {Matrix2} The matrix.
+    */
+    setFromArray (array) {
+        if (array.length === 4) {
+            this.a = array[0];
+            this.b = array[1];
+            this.c = array[2];
+            this.d = array[3];
+        }
+        else {
+            throw new Error('Cannot set matrix values from an invalid array');
+        }
+
+        return this;
+    }
+    
+    /**
+    * Sets the matrix values.
+    * @param {number} a
+    * @param {number} b
+    * @param {number} c
+    * @param {number} d
+    * @return {Matrix2} The matrix.
+    */
+    setTo (a, b, c, d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        
+        return this;
+    }
+    
+    /**
+    * Creates an array from the matrix values.
+    *
+    * @param {Array} array - If provided the matrix values will be set into the array, otherwise a new array is created.
+    * @return {Array} The array containing the matrix values.
+    */
+    toArray (array) {
+        if (!array) {
+            var array = new Array(4);    
+        }
+        
+        array[0] = this.a;
+        array[1] = this.b;
+        array[2] = this.c;
+        array[3] = this.d;
+        
+        return array;
+    }
 }
+
+/**
+* The number of columns.
+* @name Matrix2#cols
+*/
+Object.defineProperty(Matrix2.prototype, 'cols', {
+    enumerable: true,
+    value: 2
+});
+
+/**
+* The number of rows.
+* @name Matrix2#rows
+*/
+Object.defineProperty(Matrix2.prototype, 'rows', {
+    enumerable: true,
+    value: 2
+});
