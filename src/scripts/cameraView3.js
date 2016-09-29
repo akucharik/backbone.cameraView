9b17c8b6e5456a70b1fbb951b7ee1bc80144773b
Adam Kucharik <Adam@Adams-MacBook-Air.local>
2016-09-29 14:40:51 -0400

    Continue Animation rewrite: Get rid of Focuser

--- src/scripts/cameraView3.js
+++ src/scripts/cameraView3.js
@@ -82,7 +82,7 @@ var Vector2 = Oculo.Vector2;
 */
 var Camera = function (options) {
     // Compose object
-    Object.assign(this, Backbone.Events, new Focuser());
+    Object.assign(this, Backbone.Events);
     
     /**
     * @property {Object} - An object containing of all current and future animations.
@@ -542,6 +542,65 @@ p._addAnimation = function (animation) {
 p._animate = function (properties, duration, options) {
 
     return this;
+};
+
+/**
+* Get an object's position in the world.
+*
+* @param {Element} object - The object.
+* @param {Element} world - The world.
+* @returns {Vector2} The object's position.
+*/
+p.getObjectWorldPosition = function (object, world) {
+    var x = (object.offsetWidth / 2) + object.offsetLeft - world.offsetLeft; 
+    var y = (object.offsetHeight / 2) + object.offsetTop - world.offsetTop;
+
+    return new Vector2(x, y);
+};
+
+/**
+* Calculate the raw point on the scene on which the camera is positioned.
+*
+* @param {Vector2} cameraOffset - The camera's position on the scene.
+* @param {Vector2} cameraCenter - The camera's center point.
+* @param {Vector2} sceneOrigin - The scene's origin.
+* @param {Matrix2} sceneTransformation - The scene's transformation matrix.
+* @returns {Vector2} The camera's position.
+*/
+p.calculateCameraPosition = function (cameraOffset, cameraCenter, sceneOrigin, sceneTransformation) {
+    var sceneOriginOffset = sceneOrigin.clone().transform(sceneTransformation).subtract(sceneOrigin);
+
+    return cameraOffset.clone().add(sceneOriginOffset, cameraCenter).transform(sceneTransformation.getInverse());
+};
+
+/**
+* Calculate the position within the camera of the provided raw point on the scene.
+*
+* @param {Vector2} scenePosition - The raw point on the scene.
+* @param {Vector2} cameraPosition - The raw point on the scene on which the camera is positioned.
+* @param {Vector2} cameraCenter - The camera's center point.
+* @param {Matrix2} sceneTransformation - The scene's transformation matrix.
+* @returns {Vector2} The position within the camera.
+*/
+p.calculateCameraContextPosition = function (scenePosition, cameraPosition, cameraCenter, sceneTransformation) {
+    var cameraOffset = this.calculateCameraOffset(cameraPosition, cameraCenter, new Vector2(), sceneTransformation);
+
+    return scenePosition.clone().transform(sceneTransformation).subtract(cameraOffset);
+};
+
+/**
+* Calculate the camera's offset on the scene given a raw point on the scene to be placed at a point on the camera.
+*
+* @param {Vector2} scenePosition - The raw point on the scene.
+* @param {Vector2} cameraContext - The point on the camera.
+* @param {Vector2} sceneOrigin - The scene's origin.
+* @param {Matrix2} sceneTransformation - The scene's transformation matrix.
+* @returns {Vector2} The camera's offset.
+*/
+p.calculateCameraOffset = function (scenePosition, cameraContextPosition, sceneOrigin, sceneTransformation) {
+    var sceneOriginOffset = sceneOrigin.clone().transform(sceneTransformation).subtract(sceneOrigin);
+
+    return scenePosition.clone().transform(sceneTransformation).subtract(sceneOriginOffset, cameraContextPosition);
 };
 
 /**
