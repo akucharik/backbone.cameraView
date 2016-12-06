5002537c8dd554372b90367892895c4254450d97
Adam Kucharik <Adam@Adams-MacBook-Air.local>
2016-12-06 18:02:08 -0500

    Clean up build process and create separate demo build process

--- gulpfile.js
+++ gulpfile.js
@@ -15,32 +15,17 @@ var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 
 var build = {
-    demo: {
-        destDirectory: './demo/scripts',
-        destFileName: 'demo.js',
-        source: './demo/src/scripts/demo.js',
-        styles: {
-            destDirectory: './demo/styles',
-            source: './demo/src/styles/**/*.sass'
-        }
-    },
-    destDirectory:  './build',
     scripts: {
-        destDirectory: './build/scripts',
+        destDirectory: './dist',
         destFileName: 'oculo.js',
-        libraries: {
-            dest: ['./build/scripts/TweenMax.min.js', './build/scripts/Draggable.min.js', './build/scripts/ModifiersPlugin.min.js'],
-            source: ['./src/scripts/lib/TweenMax.min.js', './src/scripts/lib/Draggable.min.js', './src/scripts/lib/ModifiersPlugin.min.js']
-        },
         source: './src/scripts/oculo.js'
-    },
-    styles: {
-        destDirectory: './build/styles',
-        destFileName: 'oculo.css',
-        source: './src/styles/**/*.sass'
     },
     tests: {
         source: './tests/*.js'
+    },
+    vendor: {
+        dest: ['./dist/TweenMax.min.js', './dist/Draggable.min.js'],
+        source: ['./src/scripts/lib/TweenMax.min.js', './src/scripts/lib/Draggable.min.js']
     }
 };
 
@@ -51,7 +36,7 @@ var docs = {
 // set up default task
 gulp.task('default', ['build']);
 
-gulp.task('build', ['test:scripts', 'compile:styles', 'compile:libraries', 'compile:scripts'], function () {
+gulp.task('build', ['compile:scripts', 'test:scripts'], function () {
     return;
 });
 
@@ -59,42 +44,16 @@ gulp.task('clean:docs', function () {
     return del(docs.destDirectory);
 });
 
-gulp.task('clean:libraries', function () {
-    return del(build.scripts.libraries.dest);
-});
-
 gulp.task('clean:scripts', function () {
     return del(build.scripts.destDirectory);
 });
 
-gulp.task('clean:styles', function () {
-    return del(build.styles.destDirectory);
-});
-
-gulp.task('compile:demo', function () {
-    return browserify(build.demo.source, { debug: true })
-        .transform(babelify)
-        .bundle()
-        .on('error', function (error) { 
-            console.log('Error: ' + error.message); 
-        })
-        .pipe(source(build.demo.destFileName))
-        .pipe(gulp.dest(build.demo.destDirectory))
-        // Minify
-//        .pipe(buffer())
-//        .pipe(uglify())
-//        .pipe(rename({
-//            suffix: '.min'
-//        }))
-//        .pipe(gulp.dest(build.scripts.destDirectory));
-});
-
-gulp.task('compile:libraries', ['clean:libraries'], function () {
-    return gulp.src(build.scripts.libraries.source)
+gulp.task('copy:vendor', function () {
+    return gulp.src(build.vendor.source)
         .pipe(gulp.dest(build.scripts.destDirectory));
 });
 
-gulp.task('compile:scripts', ['clean:scripts', 'compile:libraries'], function () {
+gulp.task('compile:scripts', ['clean:scripts', 'copy:vendor'], function () {
     return browserify(build.scripts.source, { 
             debug: true,
             standalone: 'Oculo'
@@ -115,46 +74,6 @@ gulp.task('compile:scripts', ['clean:scripts', 'compile:libraries'], function ()
 //        .pipe(gulp.dest(build.scripts.destDirectory));
 });
 
-gulp.task('compile:styles', ['clean:styles'], function () {
-    return gulp.src(build.styles.source)
-        .pipe(sass({
-            includePaths: [
-                './src/styles', 
-                './node_modules/foundation-sites/scss',
-                './node_modules/normalize-scss/sass'
-            ],
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest(build.styles.destDirectory))
-        // Minify
-//        .pipe(buffer())
-//        .pipe(uglifycss())
-//        .pipe(rename({
-//            suffix: '.min'
-//        }))
-//        .pipe(gulp.dest(build.styles.destDirectory));
-});
-
-gulp.task('compile:demo:styles', ['clean:styles'], function () {
-    return gulp.src(build.demo.styles.source)
-        .pipe(sass({
-            includePaths: [
-                './demo/src/styles', 
-                './node_modules/foundation-sites/scss',
-                './node_modules/normalize-scss/sass'
-            ],
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest(build.demo.styles.destDirectory))
-        // Minify
-//        .pipe(buffer())
-//        .pipe(uglifycss())
-//        .pipe(rename({
-//            suffix: '.min'
-//        }))
-//        .pipe(gulp.dest(build.styles.destDirectory));
-});
-
 gulp.task('generate:docs', ['clean:docs'], function () {
 	executeChildProcess('./node_modules/jsdoc/jsdoc.js -c jsdocconfig.json');
 });
@@ -167,8 +86,4 @@ gulp.task('test:scripts', function () {
             },
             reporter: 'nyan'
         }));
-});
-
-gulp.task('watch:styles', ['clean:styles'], function () {
-    gulp.watch(build.styles.source, ['sass']);
 });
